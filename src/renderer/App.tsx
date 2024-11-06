88345d63617a208f1ce2e1c340961ff79f7d87c3
Ryujin42 <ryujin.hector@gmail.com>
2024-11-06 14:13:55 +0100

    feat: refacto

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,94 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Header from './Header';
+import HomeSection from './HomeSection';
+import SearchResults from './SearchResults';
+import VideoPlayer from './VideoPlayer';
 
-function Hello() {
+function Main() {
+  const API_KEY = "AIzaSyCbjwb6-OEMIhTuzWoQj5uv1WMkC8dnets";
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [homeData, setHomeData] = useState([]);
+  const [currentVideoId, setCurrentVideoId] = useState<string | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const fetchHomeData = async () => {
+    try {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          maxResults: 5,
+          q: 'trending music',
+          type: 'video',
+          videoCategoryId: '10',
+          key: API_KEY,
+        },
+      });
+      if (response.data && response.data.items) {
+        setHomeData(response.data.items);
+      }
+    } catch (error) {
+      console.error('Error fetching home data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHomeData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (searchQuery.trim() === '') {
+      setResults([]);
+      return;
+    }
+
+    try {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          maxResults: 5,
+          q: searchQuery,
+          type: 'video',
+          key: API_KEY,
+        },
+      });
+      if (response.data && response.data.items) {
+        setResults(response.data.items);
+      }
+    } catch (error) {
+      console.error('Error fetching data from YouTube API:', error);
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <Header
+        searchQuery={searchQuery}
+        handleInputChange={handleInputChange}
+        handleKeyPress={handleKeyPress}
+        handleSearch={handleSearch}
+      />
+      <main>
+        {searchQuery.trim() === '' ? (
+          <HomeSection homeData={homeData} setCurrentVideoId={setCurrentVideoId} />
+        ) : (
+          <SearchResults results={results} setCurrentVideoId={setCurrentVideoId} />
+        )}
+        {currentVideoId && <VideoPlayer currentVideoId={currentVideoId} />}
+      </main>
     </div>
   );
 }
@@ -43,8 +97,9 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
 }
+
